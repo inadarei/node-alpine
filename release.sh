185f6ef134ccd1f4e5076b164cb89ad2d19640e6
Irakli Nadareishvili <inadarei@users.noreply.github.com>
2016-06-20 23:27:33 -0400

    alpha checkin of release.sh

--- release.sh
+++ release.sh
@@ -29,11 +29,43 @@ fi
 
 
 node_version="$1"
-SEMVER_REGEX="^v[0-9]+\.[0-9]+\.[0-9]+$"
+SEMVER_REGEX="^v([0-9]+)\.([0-9]+)\.([0-9])+$"
 if [[ ! $node_version =~ $SEMVER_REGEX ]]; then
   usage;
   exit 1;
 fi
 
+re='(.*)[0-9]+(.*)'
+if [[ $node_version =~ $SEMVER_REGEX ]]; then
+  node_full_number="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}"
+  node_major_number="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}"
+fi
+
 curr_date=`date +%Y-%m-%d_%H%M`
-echo "Releasing: Node $node_version Refresh: $curr_date ";
+echo "Releasing: Node $node_version ($node_full_number/$node_major_number). Refresh: $curr_date ";
+
+sed -i.bak "s/{{NODE_VERSION}}/$node_version/" Dockerfile
+sed -i.bak "s/{{DATE_TIME}}/$curr_date/" Dockerfile
+
+# Just node
+
+git commit -m "$node_full_number" Dockerfile
+git tag -a $node_full_number -m "$node_full_number"
+git tag -d "$node_major_number" && git push origin :refs/tags/$node_major_number && git tag -a node_major_number -m "node_major_number"
+git push origin --tags
+
+# Node with runit
+sed -i.bak "s/#!RUNIT//g" Dockerfile
+ runit_suffix="-runit"
+ node_full_number="$node_full_number$runit_suffix"
+ node_major_number="$node_major_number$runit_suffix"
+
+git commit -m "$node_full_number" Dockerfile
+git tag -a $node_full_number -m "$node_full_number"
+git tag -d "$node_major_number" && git push origin :refs/tags/$node_major_number && git tag -a node_major_number -m "node_major_number"
+git push origin --tags
+
+# Cleanup
+mv Dockerfile.bak Dockerfile
+
+echo "Done!"
